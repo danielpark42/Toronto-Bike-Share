44cae16e38453354ab8fada07cf39ed2c5fc012c
danielpark42 <155781222+danielpark42@users.noreply.github.com>
2024-03-11 13:18:58 -0400

    Update Table_Structure.sql
    alter table structure after the python script compiled

--- Table_Structure.sql
+++ Table_Structure.sql
@@ -28,3 +28,19 @@ grant all privileges on database "Toronto Bike Share" to postgres
 grant all privileges on all tables in schema public to postgres
 grant usage on schema public to postgres
 grant usage on all sequences in schema public to postgres
+
+
+-- Due to errors in the data, the python script was unsuccessful importing files that had data errors
+-- The unique key constraint was removed, the date style was changed, 
+
+ALTER DATABASE "Toronto Bike Share" SET datestyle TO "ISO, MDY";
+
+ALTER TABLE bikeshare
+ALTER COLUMN start_station_id TYPE VARCHAR(50),
+ALTER COLUMN end_station_id TYPE VARCHAR(50),
+ALTER COLUMN bike_id TYPE VARCHAR(50),
+ALTER COLUMN trip_id TYPE VARCHAR(50)
+
+ALTER TABLE bikeshare
+DROP CONSTRAINT bikeshare_trip_id_key
+
