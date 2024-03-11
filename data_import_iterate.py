c77f2ac4b6254b82c612a5015141ba04c1769dd2
danielpark42 <155781222+danielpark42@users.noreply.github.com>
2024-03-10 20:18:41 -0400

    Create data_import_iterate.py
    python code to iterative through all csv files in a folder and importing all the data into the postgresql database to be manipulated

---
+++ data_import_iterate.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import psycopg2
+import subprocess
+import os
+
+# Connect to PostgreSQL
+conn = psycopg2.connect(
+    dbname="Toronto Bike Share",
+    user="postgres",
+    password="password",
+    host="localhost",
+    port="5432"
+)
+cursor = conn.cursor()
+
+# Read the spreadsheet
+csv_file_path = 'Toronto_Bikeshare_Ridership_FileNameList.csv'  # Change this to your spreadsheet file path
+df = pd.read_csv(csv_file_path,index_col = 0, header = None)
+
+# password for psql to import into database
+os.environ['PGPASSWORD'] = 'password'
+
+# Iterate through file paths
+for file_path, _ in df.iterrows():
+    # Construct the \copy command
+    copy_command = f"\\copy bikeshare FROM '{file_path}' WITH (FORMAT CSV, DELIMITER ',', HEADER true);"
+
+    psql_filepath = "C:\\Program Files\\PostgreSQL\\16\\bin\\psql.exe"
+
+    # Execute the \copy command using subprocess
+    subprocess.run([psql_filepath, "-d", "Toronto Bike Share", "-U", "postgres", "-c", copy_command])
+  
+    # Print file path to document which files ran correctly and document which files have errors
+    print(file_path)
+
+# Commit changes and close connection
+conn.commit()
+cursor.close()
+conn.close()
